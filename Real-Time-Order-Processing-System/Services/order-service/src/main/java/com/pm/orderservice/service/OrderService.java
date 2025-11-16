e7560c5f2701e50d838b3b681436d97657ebc61b
anieto1 <adnieto2019@gmail.com>
2025-11-16 13:53:06 -0500

    Add MapStruct dependency and OrderMapper implementation
    Include MapStruct library in order-service module for simplifying object mapping. Create the initial `OrderMapper` implementation for mapping Order entities to DTOs.

--- Real-Time-Order-Processing-System/Services/order-service/src/main/java/com/pm/orderservice/service/OrderService.java
+++ Real-Time-Order-Processing-System/Services/order-service/src/main/java/com/pm/orderservice/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.pm.orderservice.service;
 
 
+import com.pm.orderservice.dto.OrderRequestDTO;
+import com.pm.orderservice.model.Order;
 import com.pm.orderservice.repository.OrderItemRepository;
 import com.pm.orderservice.repository.OrderRepository;
 import com.pm.orderservice.repository.OutboxEventRepository;
@@ -14,6 +16,13 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final OrderItemRepository orderItemRepository;
     private final OutboxEventRepository outboxEventRepository;
+
+//    public OrderRequestDTO createOrder(OrderRequestDTO orderRequestDTO) {
+//        Order newOrder = orderRepository.save(Order.builder()
+//                .customerId(orderRequestDTO.getCustomerId())
+//                .build());
+//
+//    }
 
 
 
