da81c379c62d692017e0741f315951a0bb4962d8
anieto1 <adnieto2019@gmail.com>
2025-11-26 16:13:04 -0500

    Enable JPA auditing, scheduling, and add Kafka producer configuration with batching and retries

--- Real-Time-Order-Processing-System/Services/order-service/src/main/java/com/pm/orderservice/config/KafkaProducerConfig.java
+++ Real-Time-Order-Processing-System/Services/order-service/src/main/java/com/pm/orderservice/config/KafkaProducerConfig.java
@@ -1,4 +1,53 @@
 package com.pm.orderservice.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
+    private String bootstrapServers;
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> configs = new HashMap<>();
+
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+
+        configs.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);  // Prevent duplicates
+        configs.put(ProducerConfig.ACKS_CONFIG, "all");               // Wait for all replicas
+        configs.put(ProducerConfig.RETRIES_CONFIG, 3); // Retry until success
+
+
+        configs.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5); // Parallel requests
+        configs.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");        // Compress messages
+        configs.put(ProducerConfig.LINGER_MS_CONFIG, 10);                     // Batching delay
+        configs.put(ProducerConfig.BATCH_SIZE_CONFIG, 32 * 1024);             // 32KB batches
+
+        return configs;
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
