3c431af238020f1b8f2565a12809b23354f2d9ef
anieto1 <adnieto2019@gmail.com>
2026-01-09 22:39:58 -0500

    Add `ErrorResponse` model and extend `GlobalExceptionHandler`
    Introduce `ErrorResponse` model for standardized error representation in `inventory-service`. Enhance `GlobalExceptionHandler` by adding handling for `DuplicateProductException` with structured error response and logging.

---
+++ Real-Time-Order-Processing-System/Services/inventory-service/src/main/java/com/pm/inventoryservice/exception/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.pm.inventoryservice.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+}
--- Real-Time-Order-Processing-System/Services/inventory-service/src/main/java/com/pm/inventoryservice/exception/GlobalExceptionHandler.java
+++ Real-Time-Order-Processing-System/Services/inventory-service/src/main/java/com/pm/inventoryservice/exception/GlobalExceptionHandler.java
@@ -1,9 +1,48 @@
 package com.pm.inventoryservice.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicateProductException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateProductException(
+            DuplicateProductException ex, WebRequest request) {
+        log.error("Duplicate product: {}", ex.getMessage());
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.CONFLICT.value())
+                .error("Bad Request")
+                .message(ex.getMessage())
+                .path(request.getDescription(false).replace("uri=",""))
+                .build();
+
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+
+    @ExceptionHandler(InsufficientStockException.class)
+
+
+    @ExceptionHandler(InvalidReservationStateException.class)
+
+
+    @ExceptionHandler(InventoryNotFoundException.class)
+
+
+    @ExceptionHandler(ReservationNotFoundException.class)
+
+
+    @ExceptionHandler(StockOperationException.class)
 
 
 
