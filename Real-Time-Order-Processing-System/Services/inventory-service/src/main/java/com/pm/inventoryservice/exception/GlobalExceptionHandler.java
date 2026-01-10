5122cafbe651b188b8683806a52e92bd90922b3f
anieto1 <adnieto2019@gmail.com>
2026-01-09 23:10:35 -0500

    Add `ErrorResponse` model and extend `GlobalExceptionHandler`
    Introduce `ErrorResponse` model for standardized error representation in `inventory-service`. Enhance `GlobalExceptionHandler` by adding handling for `DuplicateProductException` with structured error response and logging.

--- Real-Time-Order-Processing-System/Services/inventory-service/src/main/java/com/pm/inventoryservice/exception/GlobalExceptionHandler.java
+++ Real-Time-Order-Processing-System/Services/inventory-service/src/main/java/com/pm/inventoryservice/exception/GlobalExceptionHandler.java
@@ -25,24 +25,87 @@ public class GlobalExceptionHandler {
                 .path(request.getDescription(false).replace("uri=",""))
                 .build();
 
-
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
 
     @ExceptionHandler(InsufficientStockException.class)
+    public ResponseEntity<ErrorResponse> handleInsufficientStockException(
+            InsufficientStockException ex, WebRequest request) {
+        log.error("Insufficient stock: {}", ex.getMessage());
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.CONFLICT.value())
+                .error("Bad request")
+                .message(ex.getMessage())
+                .path(request.getDescription(false).replace("uri=",""))
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
 
 
     @ExceptionHandler(InvalidReservationStateException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidReservationStateException(
+            InvalidReservationStateException ex, WebRequest request){
+        log.error("Invalid reservation state: {}", ex.getMessage());
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.CONFLICT.value())
+                .error("Bad request")
+                .message(ex.getMessage())
+                .path(request.getDescription(false).replace("uri=",""))
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
 
 
     @ExceptionHandler(InventoryNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleInventoryNotFoundException(
+            InventoryNotFoundException ex, WebRequest request){
+        log.error("Inventory not found: {}", ex.getMessage());
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND.value())
+                .error("Not Found")
+                .message(ex.getMessage())
+                .path(request.getDescription(false).replace("uri=",""))
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
 
 
     @ExceptionHandler(ReservationNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleReservationNotFoundException(
+            ReservationNotFoundException ex, WebRequest request){
+        log.error("Reservation not found: {}", ex.getMessage());
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND.value())
+                .error("Not Found")
+                .message(ex.getMessage())
+                .path(request.getDescription(false).replace("uri=",""))
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
 
 
     @ExceptionHandler(StockOperationException.class)
+    public ResponseEntity<ErrorResponse> handleStockOperationException(
+            StockOperationException ex, WebRequest request){
+        log.error("Stock operation failed:{}", ex.getMessage());
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error("Bad request")
+                .message(ex.getMessage())
+                .path(request.getDescription(false).replace("uri=",""))
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 
 
 
